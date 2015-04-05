3c07a23a7a482488d30f7c706214b926ef2f43a8
pvempa2@uic.edu <pvempa2@uic.edu>
2015-04-04 20:46:14 -0500

    pushing changes wrto linux

--- ParseJSON/resources/linux/curl-git.sh
+++ ParseJSON/resources/linux/curl-git.sh
@@ -3,7 +3,7 @@ read pageCount
 cd /home/theja/ALPFA/SearchResults && {
 for ((i = 1 ;i <= $pageCount;i++))
 do
-	curl -u pvempa2@uic.edu:d62aAPgpZ3DS -o Page$i.json "https://api.github.com/search/repositories?q=testing+language:java+NOT+android+NOT+javascript+NOT+jquery+NOT+ios+NOT+xml&sort=stars?&page=$i";
+	curl -u pvempa2@uic.edu:d62aAPgpZ3DS -o Page$i.json "https://api.github.com/search/repositories?q=java+language:java+NOT+android+NOT+javascript+NOT+jquery+NOT+ios+NOT+xml&sort=stars&page=$i";
 
 	if (( $i % 30 == 0 ))
 	then
