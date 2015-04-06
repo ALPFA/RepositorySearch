ee6747883d382ec0590f3cf8879b40cc221da9f2
pvempa2@uic.edu <pvempa2@uic.edu>
2015-04-05 23:48:04 -0500

    modified code to download tar files

--- ParseJSON/src/com/AL5/IssueParser/DownloadTarBalls.java
+++ ParseJSON/src/com/AL5/IssueParser/DownloadTarBalls.java
@@ -13,21 +13,25 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 /**
  * This program downloads source code for all versions of selected subjects.
- * The selected subjects and version details are obtained from GenerateReleaseDetailsForProjects.java
+ * The selected subjects and version details are obtained from GetProjectMetadata.java
  * @author theja
  *
  */
 public class DownloadTarBalls {
 
 	public static void main(String[] args) {
-		Properties properties = new Properties();
-		InputStream input = null;
+		Properties properties_dir = new Properties();
+		Properties properties_auth = new Properties();
+		InputStream dir_props = null;
+		InputStream auth_props = null;
 		try {
-			input = new FileInputStream("lib/linux_directories.properties");
-			properties.load(input);
+			dir_props = new FileInputStream("lib//linux_directories.properties");
+			auth_props = new FileInputStream("lib//authentication.properties");
+			properties_dir.load(dir_props);
+			properties_auth.load(auth_props);
 			//TODO: Change path to releases
-			final File folder = new File(properties.getProperty("PATH_FOR_RELEASES"));
-			listFilesForFolder(folder,properties);
+			final File folder = new File(properties_dir.getProperty("PATH_FOR_RELEASES"));
+			listFilesForFolder(folder,properties_auth,properties_dir);
 			System.out.println("Finished");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -35,15 +39,15 @@ public class DownloadTarBalls {
 		}
 	}
 	// Source: http://stackoverflow.com/a/1846349/2833048
-		public static void listFilesForFolder(final File folder,Properties properties) {
+		public static void listFilesForFolder(final File folder,Properties properties_auth,Properties properties_dir) {
 		    for (final File fileEntry : folder.listFiles()) {
 		        if (fileEntry.isDirectory()) {
-		            listFilesForFolder(fileEntry,properties);
+		            listFilesForFolder(fileEntry,properties_auth,properties_dir);
 		        } 
 		        else {
 	        		String path = fileEntry.getPath();
 	        		String tarball_URL = getTarBallURL(path);
-		            downloadSourceCode(tarball_URL,properties);
+		            downloadSourceCode(tarball_URL,properties_auth,properties_dir);
 		        }
 		    }
 		    System.out.println("-----------------End of project-------------");
@@ -72,16 +76,21 @@ public class DownloadTarBalls {
 		 * Download the actual source code to the desired directory
 		 * @param tarball_URL
 		 */
-		private static void downloadSourceCode(String tarball_URL,Properties properties) {
+		private static void downloadSourceCode(String tarball_URL,Properties properties_auth,Properties properties_dir) {
 			try{
-				URL url = new URL(tarball_URL);
+				URL url = new URL(tarball_URL+"?client_id="+properties_auth.getProperty("CLIENT_ID")+"&client_secret="+properties_auth.getProperty("CLIENT_SECRET"));
 				//TODO: Change to actual download content directory
-				String path = properties.getProperty("PATH_FOR_SUBJECTS_SOURCE_CODE");
+				String path = properties_dir.getProperty("PATH_FOR_SUBJECTS_SOURCE_CODE");
 				String[] tar_split = tarball_URL.split("/");
 				String fileName = tar_split[tar_split.length-1]+".tar";
+				String projectName = tar_split[tar_split.length-3];
 				URLConnection connection = url.openConnection();
 				InputStream in = connection.getInputStream();
-				FileOutputStream fos = new FileOutputStream(new File(path+fileName));
+				File file = new File(path+projectName+File.separator);
+				if (!file.exists()) {
+					file.mkdirs();
+				}
+				FileOutputStream fos = new FileOutputStream(new File(file.getAbsoluteFile()+File.separator+fileName));
 				byte[] buf = new byte[512];
 				while (true) {
 				    int len = in.read(buf);
