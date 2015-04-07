9c9ad8d8f8133aaec85e97771674e7997b904536
Phani Theja Swarup Vempalli <Vempalli@users.noreply.github.com>
2015-04-07 09:06:59 -0500

    Modified search query
    Changed search query for batch file. Making it consistent with linux version

--- ParseJSON/resources/windows/curl-git.bat
+++ ParseJSON/resources/windows/curl-git.bat
@@ -4,5 +4,5 @@ set password=d62aAPgpZ3DS
 set pagelimit=30
 
 FOR /l %%X IN (1,1,%pagelimit%) DO (
-	curl -u %user%:%password% -o Page%%X.json "https://api.github.com/search/repositories?q=testing+language:java+NOT+android+NOT+javascript+NOT+jquery+NOT+ios+NOT+xml&sort=stars?&page=%%X"
+	curl -u %user%:%password% -o Page%%X.json "https://api.github.com/search/repositories?q=java+language:java+NOT+android+NOT+javascript+NOT+jquery+NOT+ios+NOT+xml&sort=stars?&page=%%X"
 )
